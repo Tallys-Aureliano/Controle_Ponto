082a7da1f219d53ec115f04b54878b570c9f96ca
dev-michel324 <dev_michel324@protonamail.com>
2022-09-18 16:56:18 -0300

    feat: adicionado arquivo de parte do sidebar

---
+++ folha_de_pontos/resources/views/partials/sidebar/_sidebar_employe.blade.php
@@ -0,0 +1,32 @@
+<li class="nav-item">
+    <a class="nav-link" href=""><span>Inicio</span></a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href=""><span>Perfil</span></a>
+</li>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseOne">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Justificativas</span>
+    </a>
+    <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="buttons.html">Nova justificativa</a>
+            <a class="collapse-item" href="cards.html">Minhas justificativas</a>
+        </div>
+    </div>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href=""><span>Histórico de pontos</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+<li class="nav-item">
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <input type="submit" value="Sair">
+    </form>
+</li>

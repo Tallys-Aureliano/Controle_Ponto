f843587cad32a9617810875dbc995b2575a8de08
dev-michel324 <dev_michel324@protonamail.com>
2022-09-18 21:41:52 -0300

    feat: adicionado documento html para apresentar os dados do perfil

---
+++ folha_de_pontos/resources/views/users/employe/profile.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.dashboard')
+
+@section('title')
+Meu perfil
+@endsection
+
+@section('content-dashboard')
+
+	<h1 class="text-center">MEU PERFIL</h1>
+
+	<a href="" class="btn btn-primary">Editar</a>
+	<br>
+	<img src="{{ $user->photo }}" alt="Foto do usuario">
+	<p>Nome: {{ $user->name }}</p>
+	<p>Email: {{ $user->email }}</p>
+	<p>Matricula: {{ $user->matricula }}</p>
+	<p>Cpf: {{ $user->cpf }}</p>
+	<p>Cargo: {{ $user->position->name }}</p>
+	
+
+@endsection

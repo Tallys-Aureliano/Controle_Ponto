ce357c19c802c0c7eb750497b9cb2afa1bf8ff92
dev-michel324 <dev_michel324@protonamail.com>
2022-09-19 14:53:02 -0300

    feat: adicionado link para editar usuario no button

--- folha_de_pontos/resources/views/users/employe/profile.blade.php
+++ folha_de_pontos/resources/views/users/employe/profile.blade.php
@@ -10,7 +10,7 @@ Meu perfil
 	<div class="mt-3">
 	</div>
 	<div class="text-center mx-auto d-flex flex-column align-items-center">
-	<a href="" class="btn btn-secundary-m mt-5 ">Editar</a>
+	<a href="{{ route('manager.edit.employe', ['id'=>$user->id]) }}" class="btn btn-secundary-m mt-5 ">Editar</a>
 		<img src="{{ asset('assets/img/banner.png') }} alt="Foto do usuario" class="mb-4 mt-5" style="width: 150px; height:150px;border-radius:50%">
 		<div class="mx-auto" >
 			<ul class="list-unstyled text-left mt-5">
@@ -20,7 +20,6 @@ Meu perfil
 				<li class="mt-4"><h5>Cpf: {{ $user->cpf }}</h5></li>
 				<li class="mt-4"><h5>Cargo: {{ $user->position->name }}</h5></li>
 			</ul>
-			<a href="{{ route('manager.edit.employe', ['id'=>$user->id]) }}" class="btn btn-primary-m mt-5 btn-lg">Editar</a>
 		</div>
 	</div>
 

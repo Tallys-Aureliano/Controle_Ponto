a1667451236074616c2f83b4530ffef23a53c13b
dev-michel324 <dev_michel324@protonamail.com>
2022-09-18 21:37:32 -0300

    feat: adicionado rota para perfil do usuario

--- folha_de_pontos/routes/user/manager.php
+++ folha_de_pontos/routes/user/manager.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManagerController;
 
 Route::middleware(['auth', 'check.manager'])->group(function () {
-    Route::get('manager/dashboard', function(){
-    	return view('users.employe.dashboard');
-    })->name('manager.dashboard');
+
+    Route::get('manager/dashboard', [ManagerController::class, 'index'])
+    	->name('manager.dashboard');
+
+    Route::get('manager/profile', [ManagerController::class, 'show'])
+    	->name('manager.profile');
 });

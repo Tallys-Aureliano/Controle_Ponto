ba5cd8ade2ad5f6633169980bf30713d1c2e2e98
dev-michel324 <dev_michel324@protonamail.com>
2022-09-19 14:46:56 -0300

    adicionado link em botao para gerente editar usuario direto do perfil

--- folha_de_pontos/resources/views/users/employe/profile.blade.php
+++ folha_de_pontos/resources/views/users/employe/profile.blade.php
@@ -6,7 +6,7 @@ Meu perfil
 
 @section('content-dashboard')
 	
-	<h1 class="text-center">Meu perfil</h1>
+	<h1 class="text-center">Perfil</h1>
 	<div class="mt-3">
 	</div>
 	<div class="text-center mx-auto" style="width:400px;">
@@ -19,7 +19,7 @@ Meu perfil
 				<li class="mt-4"><h5>Cpf: {{ $user->cpf }}</h5></li>
 				<li class="mt-4"><h5>Cargo: {{ $user->position->name }}</h5></li>
 			</ul>
-			<a href="" class="btn btn-primary-m mt-5 btn-lg">Editar</a>
+			<a href="{{ route('manager.edit.employe', ['id'=>$user->id]) }}" class="btn btn-primary-m mt-5 btn-lg">Editar</a>
 			
 		</div>
 	</div>

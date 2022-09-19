e61a423d90aa91497c37aaa46e9ce13e93c2c155
dev-michel324 <dev_michel324@protonamail.com>
2022-09-18 23:08:37 -0300

    style: adicionando o conteudo da dashboard para gerente ou funcionario

--- folha_de_pontos/resources/views/users/employe/dashboard.blade.php
+++ folha_de_pontos/resources/views/users/employe/dashboard.blade.php
@@ -6,4 +6,10 @@ Dashboard - Gerente
 
 @section('content-dashboard')
 
+@if(auth()->user()->role == 1)
+	@include('partials.dashboard-content._content_manager')
+@else
+	@include('partials.sidebar._sidebar_employe')
+@endif
+
 @endsection

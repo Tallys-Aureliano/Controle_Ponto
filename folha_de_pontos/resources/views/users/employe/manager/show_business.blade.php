78281a935b36a7a6a2a3bc873481e4d1c5e48b15
dev-michel324 <dev_michel324@protonamail.com>
2022-09-19 22:55:37 -0300

    fix: remocao de link para rota que ja foi retirada pelo backend

--- folha_de_pontos/resources/views/users/employe/manager/show_business.blade.php
+++ folha_de_pontos/resources/views/users/employe/manager/show_business.blade.php
@@ -9,7 +9,6 @@ Dados da empresa
 	<div class="mt-3">
 	</div>
 		<div class="text-center mx-auto d-flex flex-column align-items-center">
-		<a href="{{ route('manager.edit.business') }}"><button class="btn btn btn-secundary-m mt-5">Editar</button></a>
 		<div class="mx-auto" >
 			<ul class="list-unstyled text-left mt-5">
 				<li class=""><h5>Nome: {{ $business->name }}</h5></li>

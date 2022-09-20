f5399af7e9717dfccc012a586891e18abb5f6529
dev-michel324 <dev_michel324@protonamail.com>
2022-09-20 14:45:10 -0300

    feat: adicionado rota para criar empresa direto da dashboard do admin

--- folha_de_pontos/resources/views/users/admin/dashboard.blade.php
+++ folha_de_pontos/resources/views/users/admin/dashboard.blade.php
@@ -9,7 +9,7 @@ Dashboard de administração
 
 	<ul class="list-group rounded-3 mb-5 mt-5">
 	  <li class="list-group-item active text-center fs-5" aria-current="true">Autenticação e Autorização</li>
-	  <li class="list-group-item d-flex justify-content-between align-items-center"><a href="{{ route('admin.business.list') }}"><p class="">Empresas</p></a><button class="btn btn-sm btn-secundary-m">Adicionar</button></li>
+	  <li class="list-group-item d-flex justify-content-between align-items-center"><a href="{{ route('admin.business.list') }}"><p class="">Empresas</p></a><a href="{{ route('admin.business.create') }}"><button class="btn btn-sm btn-secundary-m">Adicionar</button></a></li>
 	  <li class="list-group-item d-flex  justify-content-between "><a href="{{ route('admin.employe.list') }}">Funcionários</a><button class="btn btn-sm btn-secundary-m">Adicionar</button></li>
 	</ul>
 
